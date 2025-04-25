bf8109a22d7ed6106b83b46ba6c32ea07c394b45
yonatanrozin <yonatanrozin42@gmail.com>
2025-04-25 08:38:40 -0400

    update network script

--- pages/socket_script.js
+++ pages/socket_script.js
@@ -1,16 +1,34 @@
 const data = {};
-const socket = new WebSocket("/messages");
-socket.addEventListener("open", () => setInterval(() => {
-    socket.send(JSON.stringify(getOSCMsgs(data)));
-}, 1000/60));
+
+let socket;
+function socketConnect() {
+    socket = new WebSocket("/messages");
+    socket.addEventListener("open", () => setInterval(() => {
+        const OSCMsgs = getOSCMsgs(data);
+        console.log(OSCMsgs)
+        socket.send(JSON.stringify({packets: OSCMsgs}));
+    }, 1000/60));
+    socket.addEventListener("close", () => {
+        setTimeout(socketConnect, 500);
+    });
+}
+socketConnect();
+
+function getOSCArgs(args) {
+    if (!Array.isArray(args)) args = [args];
+    return args.map((value) => {
+        const type = {"boolean": "i", "number": "f", "string": "s"}[typeof value];
+        return {type, value};
+    });
+}
 
 function getOSCMsgs(obj, path = "") {
-    const msgs = {};
+    const msgs = [];
     for (const key in obj) {
         const keyPath = `${path}/${key}`;
-        if (Array.isArray(obj[key])) msgs[keyPath] = obj[key];
-        else if (typeof obj[key] === "object") Object.assign(msgs, getOSCMsgs(obj[key], keyPath));
-        else msgs[keyPath] = [obj[key]];
+        if (Array.isArray(obj[key])) msgs.push({address: keyPath, args: getOSCArgs(obj[key])});
+        else if (typeof obj[key] === "object") msgs.push(...getOSCMsgs(obj[key], keyPath));
+        else msgs.push({address: keyPath, args: getOSCArgs(obj[key])});
     }
     return msgs;
 }
