8c6cf3d5171c6c10339460fb603e259d6017a17d
yonatanrozin <yonatanrozin42@gmail.com>
2025-06-13 18:09:50 -0400

    add Arduino BLE sender/receiver example

---
+++ receivers/Arduino_BLE_Send_Receive/Arduino_BLE_Send_Receive.ino
@@ -0,0 +1,100 @@
+/*
+  Simple Arduino BLE Receiver
+  Yonatan Rozin
+
+  Create a public BLE device name (any)
+  Create your own service + characteristic UUIDs
+  Create <datatype>characteristics from UUIDs in setup()
+  
+  Check for characteristic updates in loop(), do stuff with the new values
+
+  Run sketch, check serial monitor for Arduino's local IP address
+  Use the IP address and port # to send UDP to this Arduino
+*/
+
+#include <ArduinoBLE.h> //not included with IDE - install in libary manager
+
+#include <Servo.h>
+
+#define servoPin 4
+
+char BLEDeviceName[] = ""; //give your device a name to appear in public interfaces (or not)
+
+//generate a v4 UUID here: https://www.uuidgenerator.net/version4 and enter below
+char BLEServiceUUID[] = "12345678-1234-abcd-1234-12345678abcd";
+
+//create characteristic UUIDs - one per characteristic
+//for each: use the service UUID above, modify by 1 character
+char BLECharacteristic1UUID[] = "22345678-1234-abcd-1234-12345678abcd";
+char BLECharacteristic2UUID[] = "32345678-1234-abcd-1234-12345678abcd";
+char BLECharacteristic3UUID[] = "42345678-1234-abcd-1234-12345678abcd";
+
+
+//create 1 ble service
+BLEService ble(BLEServiceUUID); 
+
+//create BLE characteristics - remove read/write permissions as needed
+//recommended: use BLEWriteWithoutResponse for much faster writing!
+BLEByteCharacteristic characteristic1(BLECharacteristic1UUID, BLERead | BLENotify | BLEWriteWithoutResponse); 
+BLEFloatCharacteristic characteristic2(BLECharacteristic2UUID, BLERead | BLENotify | BLEWriteWithoutResponse); 
+BLEFloatCharacteristic characteristic3(BLECharacteristic3UUID, BLERead | BLENotify | BLEWriteWithoutResponse); 
+// BLEIntCharacteristic, BLEFloatCharacteristic or BLEByteCharacteristic (for booleans)
+
+Servo servo;
+
+void setup() {
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  servo.attach(servoPin);
+
+  Serial.begin(9600);
+  if (!Serial) delay(2000);
+  if (!BLE.begin()) {
+    Serial.println("starting Bluetooth® Low Energy module failed!");
+    while (1);
+  }
+
+
+  BLE.setLocalName(BLEDeviceName); 
+
+  BLE.setAdvertisedService(ble);
+
+  ble.addCharacteristic(characteristic1);
+  ble.addCharacteristic(characteristic2);
+  ble.addCharacteristic(characteristic3);
+
+  //repeat for all characteristics
+
+  BLE.addService(ble);
+  BLE.advertise();
+
+  Serial.println("BLE device active, waiting for connections...");
+  
+}
+
+void loop(){ 
+  BLE.poll(); //check for ble events
+
+  //check characteristic for new value
+  if (characteristic1.written()) {
+    bool val = characteristic1.value();
+    digitalWrite(LED_BUILTIN, val);
+  }
+  
+  if (characteristic2.written()) {
+    float val = characteristic2.value(); 
+    Serial.print("Slider: ");
+    Serial.println(val);
+    //assuming an input between 0 and 1
+    float servoVal = val * 180;
+    servo.write(servoVal);
+  }
+
+  if (millis() % 1000 < 30) {
+    characteristic3.writeValue((float)random(0, 100) / 100.0);
+    delay(20);
+  }
+
+  delay (10);
+}
+
